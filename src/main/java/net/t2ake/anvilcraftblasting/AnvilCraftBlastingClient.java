5119cac67f31351294e45f155617cbbe7a6fd332
t2ake <sunsunsll@outlook.com>
2024-03-27 12:32:35 +0800

    Initial commit

---
+++ src/main/java/net/t2ake/anvilcraftblasting/AnvilCraftBlastingClient.java
@@ -0,0 +1,10 @@
+package net.t2ake.anvilcraftblasting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnvilCraftBlastingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/t2ake/anvilcraftblasting/AnvilCraftBlastingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.t2ake.anvilcraftblasting;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnvilCraftBlastingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
